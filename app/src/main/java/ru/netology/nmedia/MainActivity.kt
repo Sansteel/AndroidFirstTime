30e54b0b2271641332e6475236d6bfcba4575926
Alexander <111@111.ru>
2022-10-02 22:53:22 +0300

    надпись поменял + добавил  кнопку и отработку нажати яна кнопку

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,10 +2,18 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onMyClick(view: View) {
+        Toast.makeText(applicationContext, "Click", Toast.LENGTH_LONG).show();
+
     }
 }
